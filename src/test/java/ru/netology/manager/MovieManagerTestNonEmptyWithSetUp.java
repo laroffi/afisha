5b2bf79c344bc0c5a172155c99fd2618fb26662c
Larisa Pylaeva <laralord@yandex.ru>
2021-05-24 02:09:07 +0300

    Second try

--- src/test/java/ru/netology/manager/MovieManagerTestNonEmptyWithSetUp.java
+++ src/test/java/ru/netology/manager/MovieManagerTestNonEmptyWithSetUp.java
@@ -13,6 +13,12 @@ class CartManagerTestNonEmptyWithSetup {
     private MovieItem third = new MovieItem(3, 3, "Love Actually", 1);
     private MovieItem fourth = new MovieItem(4, 4, "VHS", 1);
     private MovieItem fifth = new MovieItem(5, 5, "SAW2", 1);
+    private MovieItem sixth = new MovieItem(6, 6, "Frozen", 1);
+    private MovieItem seventh = new MovieItem(7, 7, "FrozenII", 1);
+    private MovieItem eighth = new MovieItem(8, 8, "Loosers", 1);
+    private MovieItem ninth = new MovieItem(9, 9, "Twins", 1);
+    private MovieItem tenth = new MovieItem(10, 10, "Friends", 1);
+
 
     @BeforeEach
     public void setUp() {
@@ -21,15 +27,21 @@ class CartManagerTestNonEmptyWithSetup {
         manager.add(third);
         manager.add(fourth);
         manager.add(fifth);
+        manager.add(sixth);
+        manager.add(seventh);
+        manager.add(eighth);
+        manager.add(ninth);
+        manager.add(tenth);
     }
 
     @Test
     public void shouldRemoveIfExists() {
+
         int idToRemove = 1;
         manager.removeById(idToRemove);
 
         MovieItem[] actual = manager.getAll();
-        MovieItem[] expected = new MovieItem[]{third, second};
+        MovieItem[] expected = new MovieItem[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second};
 
 //    assertEquals(expected, actual);
         assertArrayEquals(expected, actual);
@@ -43,6 +55,8 @@ class CartManagerTestNonEmptyWithSetup {
         MovieItem[] expected = new MovieItem[]{fourth, third, second, first};
 
     }
+
+
 
 
 }
